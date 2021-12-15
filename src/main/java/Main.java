b3e24aa4fdef04cfdec02a1cbf0fd25465e81412
Quicano <33662621+Quicano@users.noreply.github.com>
2021-12-15 10:57:06 +0100

    Fully Added Poppler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,40 @@
 import classifier.Classifier;
 import classifier.Category;
+import util.PDFToXML;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
-    public static List<File> readXMLFiles(){
-        File DataDir = new File("Data");
-        ArrayList<File> xmlList = new ArrayList<>(Arrays.asList(DataDir.listFiles()));
-        System.out.println(xmlList);
 
+    public static ArrayList<File> convertPDFFiles() throws Exception {
+        File DataDir = new File("InputData");
+        ArrayList<File> fileList = new ArrayList<>(Arrays.asList(DataDir.listFiles()));
+        ArrayList<File> outputList = new ArrayList<>();
 
-        return xmlList;
+        PDFToXML converter = new PDFToXML();
+        for (File f : fileList){
+            String fileName = f.getName();
+            if(f.getName().endsWith(".tmp") || f.getName().endsWith(".xml")){
+                outputList.add(f);
+            }
+            if(f.getName().endsWith(".pdf")){
+                String xmlPath = converter.convertToXml("Data/"+f.getName());
+                outputList.add(new File("InputData/"+ outputList));
+            }
+        }
+        return outputList;
     }
 
     public static void main(String[] args) throws Exception {
-        List<File> fileNames = readXMLFiles();
+        List<File> fileNames = convertPDFFiles();
         for (File f : fileNames){
             Category category = Classifier.asignToCategory(f);
             System.out.println("Die Datei " + f.getName() + " ist von der Kategorie: " + category);
         }
-
     }
 }
